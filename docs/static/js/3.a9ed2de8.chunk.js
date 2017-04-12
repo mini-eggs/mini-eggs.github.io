ca418219a068b017e5ca1a51a3ff0c9f9c46183f
Evan Jones <evanjones4040@gmail.com>
2017-04-11 18:03:50 -0700

    removing docs folder

--- docs/static/js/3.a9ed2de8.chunk.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([3],{451:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var o=n(5),r=n.n(o),l=n(177);t.default=function(){return r.a.createElement(l.b,{to:"/",style:{color:"white",textAlign:"center",textDecoration:"none",display:"flex",justifyContent:"center",alignItems:"center",height:"50vh"}},"Page not found,",r.a.createElement("br",null),"go home?")}}});
-//# sourceMappingURL=3.a9ed2de8.chunk.js.map
