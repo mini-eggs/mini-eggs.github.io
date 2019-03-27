40a90d111fea646f21bb64fe6f6b79a132c3629c
Evan M Jones <evan@crowdhubapps.com>
2019-03-26 19:10:04 -0700

    Fix(Dev server): Add env vars while running dev server locally.

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,3 +1,8 @@
+export SPACE_ID=$(cat now.json | jq -r '.env.SPACE_ID')
+export ACCESS_TOKEN=$(cat now.json | jq -r '.env.ACCESS_TOKEN')
+export HOME_ID=$(cat now.json | jq -r '.env.HOME_ID')
+export SIDEBAR_ID=$(cat now.json | jq -r '.env.SIDEBAR_ID')
+
 cat main.go | sed s/package\ handler/package\ main/ > main.go.tmp
 mv main.go.tmp main.go
 npm start
